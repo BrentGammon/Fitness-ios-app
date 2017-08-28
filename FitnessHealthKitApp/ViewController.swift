230f2cf5f8105c5c1f8da80c1a6f6cdd61895c21
Brent <brentgammon94@gmail.com>
2017-08-28 20:04:28 +0100

    Initial Commit

---
+++ FitnessHealthKitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitnessHealthKitApp
+//
+//  Created by Brent Gammon on 28/08/2017.
+//  Copyright © 2017 Brent Gammon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
